c035c43daa890965986cba00cbbdab23870bc0dd
Rahul-Sarang <ars.rahul.san@gmail.com>
2026-01-04 22:47:51 +0530

    Time: 18 ms (23.19%), Space: 12.5 MB (52.55%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,10 +1,16 @@
- 
 class Solution(object):
     def isPalindrome(self, x):
-        s = str(x)
-        rev = ""
+        if x < 0:
+            return False
+        realX = x
+        rev = 0
+        while x > 0:
+            num = x % 10
+            rev = rev * 10 + num
+            x = x // 10
         
-        for i in range(len(s) - 1, -1, -1):
-            rev += s[i]
-        
-        return s == rev
+        return realX == rev
+obj = Solution()
+print(obj.isPalindrome(121))  
+print(obj.isPalindrome(123))   
+print(obj.isPalindrome(-121)) 
